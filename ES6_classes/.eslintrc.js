c4ddbf3b9aea6fd7576457237044f43fbd38e140
Fahad <10679@holbertonstudents.com>
2025-08-21 13:41:31 +0300

    Update .eslintrc.js

--- ES6_classes/.eslintrc.js
+++ ES6_classes/.eslintrc.js
@@ -1,29 +1,26 @@
-// .eslintrc.js
 module.exports = {
-  root: true,
   env: {
+    browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-  },
-  extends: ['eslint:recommended'],
-  // Apply Jest-specific config only to test files
-  overrides: [
-    {
-      files: [
-        '**/__tests__/**/*.[jt]s?(x)',
-        '**/?(*.)+(spec|test).[tj]s?(x)',
-      ],
-      env: { jest: true },
-      plugins: ['jest'],
-      extends: ['plugin:jest/recommended'],
-      rules: {
-        // You can keep this on if you want, but turning it off avoids the crash in some setups
-        'jest/no-large-snapshots': 'off',
-      },
-    },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    // Добавьте ваши правила ESLint здесь
+  }
 };
