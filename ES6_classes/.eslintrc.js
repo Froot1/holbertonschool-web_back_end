0231a7df0189daabe8741db55f67f04c0d35a4d6
Fahad <10679@holbertonstudents.com>
2025-08-21 13:56:09 +0300

    Update .eslintrc.js

--- ES6_classes/.eslintrc.js
+++ ES6_classes/.eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   plugins: ['jest'],
   rules: {
     'max-classes-per-file': 'off',
+    'jest/no-large-snapshots': 'off',
     'no-underscore-dangle': 'off',
     'no-console': 'off',
     'no-shadow': 'off',
