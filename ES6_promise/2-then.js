d42d7af9bb5695a8ee91ecb4fb35c4a1729cae0a
root <10679@holbertonstudents.com>
2025-08-30 10:35:37 +0000

    2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,11 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({
-      status: 200,
-      body: 'success',
-    }))
-    .catch(() => Error())
-    .finally(() => console.warn('Got a response from the API'));
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return Error();
+    });
 }
-
-export default handleResponseFromAPI;
